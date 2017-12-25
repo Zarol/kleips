e18a01f5300e36dacddf4fc1d50103de7ec6dfdc
zarol <saharathkleips@gmail.com>
2017-12-24 18:18:51 -0800

    Added npm-script for robotstxt

---
+++ scripts/generate-robotstxt.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const robotstxt = require("generate-robotstxt").default;
+
+async function main() {
+  let content;
+
+  switch (process.argv[2]) {
+    case 'staging':
+      content = await robotstxt({
+        policy: [{
+          userAgent: "*",
+          disallow: "/",
+        }]
+      });
+      break;
+    case 'prod':
+      content = await robotstxt({
+        policy: [{
+          userAgent: "*",
+          allow: "/",
+        }],
+        sitemap: "https://www.kleips.com/sitemap.xml",
+        host: "www.kleips.com"
+      });
+      break;
+    case 'dev':
+    default:
+      content = await robotstxt({
+        policy: [{
+          userAgent: "*",
+          disallow: "/",
+        }],
+        sitemap: "http://localhost:4000/sitemap.xml",
+        host: "localhost:4000"
+      });
+      break;
+  }
+
+  fs.writeFile("public/robots.txt", content, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("The robots.txt file was saved!");
+  });
+}
+
+main();
